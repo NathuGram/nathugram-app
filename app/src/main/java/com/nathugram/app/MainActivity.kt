86f8c2b0f574556d274f241c9992884b8e8996d2
NathuGram <nathugram95@gmail.com>
2026-09-03 22:24:07 +0530

    Create MainActivity with WebView functionality
    Implement MainActivity with WebView to load a URL.

---
+++ app/src/main/java/com/nathugram/app/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.nathugram.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl("http://nathugram.xyz/Script")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
